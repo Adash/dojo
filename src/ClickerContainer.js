a5c8ccef8c72de817231b5346bf33dcc9a5af4a8
Adash <adash108@yahoo.com>
2019-01-25 11:23:08 +0000

    Fixed reducer, added new button off and renamed switch to on

--- src/ClickerContainer.js
+++ src/ClickerContainer.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
-import { createStore } from 'redux';
-import './Clicker.css';
+import React, { Component } from "react";
+import { createStore } from "redux";
+import "./Clicker.css";
 
-let switchLed = (state = 'led-gray', action)=>{
-  switch(action.type){
-    case 'FLIPIT' :
-      return (state ==='led-gray' ? 'led-green' : 'led-gray')
-    default :
-      console.log('unregistered command');
+let ON = {type:'ON', text: 'whoo'};
+let OFF = {type:'OFF', text: 'whoo'};
+
+function switchLed(state, action){
+  if (typeof state === "undefined") {
+    return "led-gray";
   }
-}
+  switch(action.type) {
+    case ("ON") :
+      console.log(action.type);
+      return "led-green"
+    case ("OFF") :
+      console.log(action.type);
+      return "led-gray"
+    default:
+      return "no state";
+  }
+};
+
 
 let store = createStore(switchLed);
 //store.subscribe(() => console.log(store.getState()))
@@ -19,69 +30,83 @@ class ClickerContainer extends Component {
     super(props);
 
     this.state = {
-      textField1: '',
+      textField: "",
       clicker: false,
       numClicks: 0,
-      led: 'led-gray',
-    }
+      led: "led-gray"
+    };
 
     this.handleNewValueChange = this.handleNewValueChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
     this.triggerAction = this.triggerAction.bind(this);
+    this.handleClick = this.handleClick.bind(this);
 
-    this.textInput = React.createRef();
+    //this.textInput = React.createRef();
   }
 
   handleNewValueChange(event) {
     const value = event.target.value;
     const fieldName = event.target.name;
     this.setState({
-      [fieldName]: value,
+      [fieldName]: value
     });
   }
 
-  handleClick(){
-    console.log(this.textInput.current);
-    let curNumClicks = this.state.numClicks;
-    this.setState({numClicks: curNumClicks + 1});
-    this.textInput.current.focus();
-  }
+   handleClick() {
+     //console.log(this.textInput.current);
+     let curNumClicks = this.state.numClicks;
+     this.setState({ numClicks: curNumClicks + 1 });
+     console.log(this.state.numClicks);
+     //this.textInput.current.focus();
+    }
 
-  triggerAction() {
-    store.getState();
-    //this.setState({led: (this.state.led ==='led-gray' ? 'led-green' : 'led-gray')});
-    store.dispatch({ type: 'FLIPIT' });
+  triggerAction(Action) { 
+    //console.log(store.getState())
+    store.dispatch(Action);
+    this.forceUpdate()
   }
 
   render() {
-    const {textValue} = this.state;
+    const { textValue } = this.state;
     const ledState = store.getState();
     return (
-        <div className='ClickerWrapper'>
-          <div className='col-3'>
-            <input 
-                className='mt-3'
-                type='text' 
-                name='textField' 
-                value={textValue} 
-                onChange={this.handleNewValueChange} 
-                ref={this.textInput} 
-            />
-            <button 
-              className='btn btn-outline-info m-2' 
-              onClick={this.handleClick}
-            >Click Me</button>
-            <button 
-              className='btn btn-outline-info m-2' 
-              onClick={this.triggerAction}
-              >Switch</button>
-          </div>
-          <div className='col-3'></div>
-          <div className='col-3'>
-            <div className={ledState}></div>
-          </div>
+      <div className="ClickerWrapper">
+        <div className="col-3">
+          <input
+            className="mt-3"
+            type="text"
+            name="textField"
+            value={textValue}
+            onChange={this.handleNewValueChange}
+            ref={this.textInput}
+          />
+
+          <button
+            className="btn btn-outline-primary m-2"
+            onClick={this.handleClick}
+          >
+            Count
+          </button>
+          <button
+            className="btn btn-outline-info m-2"
+            onClick={()=>this.triggerAction(ON)}
+          >
+            On
+          </button>
+          <button
+            className="btn btn-outline-info m-2"
+            onClick={()=>this.triggerAction(OFF)}
+          >
+            Off
+          </button>
         </div>
-    )
+        <div className="col-3" />
+        <div className="col-3">
+          <div className={ledState} />
+          <div className={ledState} />
+          <div className={ledState} />
+        </div>
+      </div>
+    );
   }
 }
 
